dfae5a97108c41a21772c949e9544bdd760fd7b1
Sahil181045 <sahilkadam257@gmail.com>
2022-11-04 21:04:27 +0530

    First commit

---
+++ classes/client.py
@@ -0,0 +1,40 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+omds = myclient["omds"]
+users = omds["users"]
+
+
+class Client:
+    def __init__(self, email, password, first_name=None, last_name=None, phone=None, address=None) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.password = password
+
+    def register(self):
+        user = users.find_one({"email": self.email})
+        if user:
+            return "already_exists"
+        document = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "phone": self.phone,
+            "address": self.address,
+            "password": self.password
+        }
+        users.insert_one(document)
+        return "success"
+
+    def authenticate(self):
+        user = users.find_one({"email": self.email})
+        if not user:
+            return "no_user"
+        if user["password"] == self.password:
+            return "success"
+        else:
+            return "failure"
---
+++ pages/login.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+from classes.client import *
+
+st.header("User Login")
+email = st.text_input("E-mail", key="email_login")
+password = st.text_input("Password", type="password", key="password_login")
+clicked = st.button("Login")
+clicked_register = st.button("New user? Click here to register!")
+if clicked_register:
+    switch_page("register")
+if clicked:
+    if email == "":
+        st.error("E-mail is a required field!")
+    if password == "":
+        st.error("Password is a required field!")
+    else:
+        client = Client(email, password)
+        result = client.authenticate()
+        if result == "no_user":
+            st.error("User does not exist!")
+        elif result == "failure":
+            st.error("Incorrect password!")
+        else:
+            st.success("Login successful!")
